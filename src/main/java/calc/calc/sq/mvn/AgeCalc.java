9b2c02c10912de71039ef36a815f9cd1fbd0d1dc
sujit.panigrahi <sujit.panigrahi@BDC7-L-5296P5Y.dir.svc.accenture.com>
2020-02-14 12:07:07 +0530

    Commit for AgeCalc

--- src/main/java/calc/calc/sq/mvn/AgeCalc.java
+++ src/main/java/calc/calc/sq/mvn/AgeCalc.java
@@ -1,7 +1,6 @@
 package calc.calc.sq.mvn;
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Arrays;
 import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
@@ -9,7 +8,7 @@ import java.io.ObjectOutput;
 import java.time.DateTimeException;
 
 public class AgeCalc implements Externalizable {
-	private String inputDate;
+	 String inputDate;
 	public AgeCalc(String inputDate){
 		this.inputDate = inputDate;
 	}
@@ -31,7 +30,7 @@ public class AgeCalc implements Externalizable {
 		    	throw new DateTimeException("Invalid day exception");
 		    }
 		    if(months < 0){
-		    	sum=++sum;
+		    	//sum=++sum;
 		    	System.out.println(obj.getClass());
 		    	throw new DateTimeException("Invalid month exception");
 		    }
